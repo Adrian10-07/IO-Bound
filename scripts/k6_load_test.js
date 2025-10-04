d53526c6f2ab6fad9b6c013447577da294660ed7
Fernandovm13 <233422@ids.upchiapas.edu.mx>
2025-10-04 08:39:41 -0600

    prueba 2

--- scripts/k6_load_test.js
+++ scripts/k6_load_test.js
@@ -6,11 +6,15 @@ export const options = {
   duration: __ENV.K6_DURATION || '1m',
 };
 
-const imgPath = __ENV.IMAGE_PATH || './test-image.jpg';
+const imgPath = __ENV.IMAGE_PATH || '../images/test-image.jpg';
 const image = open(imgPath, 'b');
 
 export default function () {
-  let res = http.post('http://localhost:3000/upload', { image: http.file(image, 'test.jpg') }, { tags: { name: 'upload' } });
+  const res = http.post(
+    'http://localhost:3000/upload',
+    { image: http.file(image, 'test.jpg') },
+    { tags: { name: 'upload' } }
+  );
   check(res, { 'upload ok': (r) => r.status === 201 || r.status === 200 });
 
   const getRes = http.get('http://localhost:3000/image/test.jpg', { tags: { name: 'get' } });
